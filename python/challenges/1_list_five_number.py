0c457e5e279969d7f0af72df52458151c71aa016
Rifzky Nurman <rifzkynurman11@gmail.com>
2025-10-29 10:32:25 +0700

    Update Challange 1

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -3,13 +3,15 @@ numbers = [10, 25, 7, 40, 15]
 print("List awal:", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
-print("Angka pertama:")
-print("Angka terakhir:")
+print(f"Angka pertama: {numbers[0]}")
+print(f"Angka terakhir: {numbers[-1]}")
 
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
+numbers.append(11)
 print("List setelah ditambah:", numbers)
 
 # 4. TODO: Urutkan list secara descending
 
 # 5. Tampilkan hasil akhir
+numbers.sort(reverse=True)
 print("List urut descending:", numbers)
